d91312952f80bbec7bfe16762409559752eb8fc9
stackk1 <astackk@gmail.com>
2023-06-04 16:52:54 -0700

    Initial Commit

---
+++ ebookreader/ebookreaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebookreaderApp.swift
+//  ebookreader
+//
+//  Created by Andrew on 2023-06-04.
+//
+
+import SwiftUI
+
+@main
+struct ebookreaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
